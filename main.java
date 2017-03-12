221d61f01357ca61d8d79cdc00dfebb1bbf28b88
DawsonGeist <Dawson.geist@gmail.com>
2017-03-12 08:17:33 -0400

    Add files via upload

--- main.java
+++ main.java
@@ -12,6 +12,42 @@ public class main
 		//this'll create and add my nodes
 		Node greek0 = new Node(0,"Alpha");
 		greekAlphabet.add(greek0);
-		System.out.println(greek0.toString());
+		Node greek1 = new Node(1,"Beta");
+		greekAlphabet.add(greek1);
+		Node greek2 = new Node(2,"Gamma");
+		greekAlphabet.add(greek2);
+		Node greek3 = new Node(3,"Delta");
+		greekAlphabet.add(greek3);
+		Node greek4 = new Node(4,"Epsilon");
+		greekAlphabet.add(greek4);
+		Node greek5 = new Node(5,"Zeta");
+		greekAlphabet.add(greek5);
+		
+		//This will test ascend and descend methods:
+		System.out.println("Ascend:");
+		greekAlphabet.ascendList();
+		System.out.println("Descend:");
+		greekAlphabet.descendList();
+		
+		//Find a node
+		System.out.println("\nDoes Gamma exist in this list?");
+		if(greekAlphabet.searchNodeString("Gamma"))
+		{
+			System.out.println("Yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+		
+		System.out.println("Does Psi exist in this list?");
+		if(greekAlphabet.searchNodeString("Psi"))
+		{
+			System.out.println("Yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
 	}
 }
