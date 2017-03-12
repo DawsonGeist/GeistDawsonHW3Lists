f57356c3efb4dcd81ee8b25436f1a83e362447a8
DawsonGeist <Dawson.geist@gmail.com>
2017-03-12 06:47:58 -0400

    Add files via upload

--- main.java
+++ main.java
@@ -1,5 +1,135 @@
 package hw3;
 
-public class main {
-
+public class main 
+{
+	public static void main(String[] args)
+	{
+		//this will be the class used to test methods found in the list class and the nodes 
+		//that will will exist within the list
+		String temp = "";
+		//this'll create my nodes
+		for(int i=0;i<24;i++)
+		{
+			if(i==0)
+			{
+				temp = "Alpha";
+				Node greek0 = new Node(i,temp);
+			}
+			if(i==1)
+			{
+				temp = "Beta";
+				Node greek1 = new Node(i,temp);
+			}
+			if(i==2)
+			{
+				temp = "Gamma";
+				Node greek2 = new Node(i,temp);
+			}
+			if(i==3)
+			{
+				temp = "Delta";
+				Node greek3 = new Node(i,temp);
+			}
+			if(i==4)
+			{
+				temp = "Epsilon";
+				Node greek4 = new Node(i,temp);
+			}
+			if(i==5)
+			{
+				temp = "Zeta";
+				Node greek5 = new Node(i,temp);
+			}
+			if(i==6)
+			{
+				temp = "Eta";
+				Node greek6 = new Node(i,temp);
+			}
+			if(i==7)
+			{
+				temp = "Theta";
+				Node greek7 = new Node(i,temp);
+			}
+			if(i==8)
+			{
+				temp = "Iota";
+				Node greek8 = new Node(i,temp);
+			}
+			if(i==9)
+			{
+				temp = "Kappa";
+				Node greek9 = new Node(i,temp);
+			}
+			if(i==10)
+			{
+				temp = "Lambda";
+				Node greek10 = new Node(i,temp);
+			}
+			if(i==11)
+			{
+				temp = "Mu";
+				Node greek11 = new Node(i,temp);
+			}
+			if(i==12)
+			{
+				temp = "Nu";
+				Node greek12 = new Node(i,temp);
+			}
+			if(i==13)
+			{
+				temp = "Xi";
+				Node greek13 = new Node(i,temp);
+			}
+			if(i==14)
+			{
+				temp = "Omicron";
+				Node greek14 = new Node(i,temp);
+			}
+			if(i==15)
+			{
+				temp = "Pi";
+				Node greek15 = new Node(i,temp);
+			}
+			if(i==16)
+			{
+				temp = "Rho";
+				Node greek16 = new Node(i,temp);
+			}
+			if(i==17)
+			{
+				temp = "Sigma";
+				Node greek17 = new Node(i,temp);
+			}
+			if(i==18)
+			{
+				temp = "Tau";
+				Node greek18 = new Node(i,temp);
+			}
+			if(i==19)
+			{
+				temp = "Upsilon";
+				Node greek19 = new Node(i,temp);
+			}
+			if(i==20)
+			{
+				temp = "Phi";
+				Node greek20 = new Node(i,temp);
+			}
+			if(i==21)
+			{
+				temp = "Chi";
+				Node greek21 = new Node(i,temp);
+			}
+			if(i==22)
+			{
+				temp = "Psi";
+				Node greek22 = new Node(i,temp);
+			}
+			if(i==23)
+			{
+				temp = "Omega";
+				Node greek23 = new Node(i,temp);
+			}
+		}
+	}
 }
